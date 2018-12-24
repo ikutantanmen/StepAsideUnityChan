f9591a6bfcc429c00fc47e85114f513b8556d264
ikutashunya <s.ikuta1008@gmail.com>
2018-12-24 19:39:52 +0900

    StepAsideUnityChanプロジェクト
    変更点：ItemDestroyerのスクリプト

--- Assets/ItemDestroyer.cs
+++ Assets/ItemDestroyer.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemDestroyer : MonoBehaviour {
+public class ItemDestroyer : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //カメラのオブジェクト
+    private GameObject maincamera;
+    //Unityちゃんとカメラの距離
+    private float difference;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-    }
-
-    //画面外に出たオブジェクトをDestroy関数で破棄
-    void OnBecameInvisible()
+    // Use this for initialization
+    void Start()
     {
-        Destroy(this.gameObject);
+        //カメラのオブジェクトを取得
+        this.maincamera = GameObject.Find("Main Camera");
     }
-}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //オブジェクトが画面外に出た場合
+        if (this.transform.position.z < maincamera.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}  
